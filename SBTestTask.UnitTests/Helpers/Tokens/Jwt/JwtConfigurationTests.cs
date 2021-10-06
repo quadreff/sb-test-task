6d62b0a376509cfe2a0f586bc67fecbd5991c59f
Roman Kovalenko <quadreff.rk@gmail.com>
2021-10-06 20:06:35 +0300

    JwtConfiguration: red test

--- SBTestTask.UnitTests/Helpers/Tokens/Jwt/JwtConfigurationTests.cs
+++ SBTestTask.UnitTests/Helpers/Tokens/Jwt/JwtConfigurationTests.cs
@@ -1,12 +1,33 @@
-﻿using System.Runtime.InteropServices.ComTypes;
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using SBTestTask.WebApi.Helpers.Tokens.Jwt;
 
 namespace SBTestTask.UnitTests.Helpers.Tokens.Jwt
 {
     public class JwtConfigurationTests
     {
-        public JwtConfigurationTests()
-        {
+        private readonly Mock<IConfiguration> _configurationMock = new Mock<IConfiguration>();
 
+        [Fact]
+        public void JwtConfiguration_RetrievesProperSecret_ReturnsProperSecret()
+        {
+            // arrange
+            var expectedValue = "test";
+            _configurationMock
+                .SetupGet(x => x[JwtConfiguration.SecretPath])
+                .Returns(expectedValue)
+                .Verifiable();
+
+            // act
+            var sut = new JwtConfiguration(_configurationMock.Object);
+            var actualResult = sut.Get();
+
+            // assert
+            _configurationMock.Verify();
+            actualResult.Secret.Should().BeEquivalentTo(expectedValue);
         }
     }
 }
