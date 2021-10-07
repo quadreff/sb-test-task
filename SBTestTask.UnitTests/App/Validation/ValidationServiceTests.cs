9cdd0f70d15e30a19c78edf38afad8acb1aba671
Roman Kovalenko <quadreff.rk@gmail.com>
2021-10-07 11:15:15 +0300

    ValidationService: red test

--- SBTestTask.UnitTests/App/Validation/ValidationServiceTests.cs
+++ SBTestTask.UnitTests/App/Validation/ValidationServiceTests.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Moq;
 using SBTestTask.WebApi.App.Validation;
+using SBTestTask.WebApi.Models;
+using Xunit;
 
 namespace SBTestTask.UnitTests.App.Validation
 {
     public class ValidationServiceTests
     {
-        private readonly Mock<IConfiguration> _configurationMock;
+        private readonly Mock<IConfiguration> _configurationMock = new Mock<IConfiguration>();
         private readonly ValidationService _sut;
 
         public ValidationServiceTests()
@@ -14,11 +16,23 @@ namespace SBTestTask.UnitTests.App.Validation
             _sut = new ValidationService(_configurationMock.Object);
         }
 
-        public void Validate_DoNotThrow_IfCredentialsAreValid()
+        [Fact]
+        public void Validate_DoNotThrow_IfUsernameIsValid()
         {
             // arrange
+            var authInfo = new AuthInfo
+            {
+                Username = "test",
+                Password = "test"
+            };
+
+            _configurationMock.Setup(x => x[ValidationService.UsernamePath]).Returns("123").Verifiable();
+            
             // act
+            _sut.Validate(authInfo);
+
             // assert
+            _configurationMock.Verify();
         }
     }
 }
--- WebApi/App/Validation/ValidationService.cs
+++ WebApi/App/Validation/ValidationService.cs
@@ -9,9 +9,9 @@ namespace SBTestTask.WebApi.App.Validation
         private readonly IConfiguration _configuration;
 
         public const string Separator = Constants.ConfigurationSeparator;
-        private const string CredentialsPath = "TestCredentials";
-        private const string UsernamePath = CredentialsPath + Separator + "Username";
-        private const string PasswordPath = CredentialsPath + Separator + "Password";
+        public const string CredentialsPath = "TestCredentials";
+        public const string UsernamePath = CredentialsPath + Separator + "Username";
+        public const string PasswordPath = CredentialsPath + Separator + "Password";
 
         public ValidationService(IConfiguration configuration)
         {
