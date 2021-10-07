9ab3f28c0165358138f5d0df2c301d528a62225e
Roman Kovalenko <quadreff.rk@gmail.com>
2021-10-07 21:15:48 +0300

    Refactoring leftovers + docker compose: force web api to use specific port

--- ConsoleListener/Program.cs
+++ ConsoleListener/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Text;
+using SBTestTask.Common.Infrastructure;
+using SBTestTask.Common.Infrastructure.Mongo;
+using SBTestTask.Common.Infrastructure.RabbitMq;
+using SBTestTask.Common.Models;
 
 namespace SBTestTask.ConsoleListener
 {
-    class Program
+    public class LocalMongoConfiguration : IMongoDbConfiguration
     {
-        static void Main(string[] args)
+        public string GetConnectionString()
         {
-            Console.WriteLine("Hello World!");
+            return "mongodb://myuser:mypassword@localhost";
+        }
+    }
+
+    public class Program
+    {
+        private static void Main()
+        {
+            var rabbitMqQueue = new RabbitQueue();
+            var userRepository = new UserRepository(new MongoDbContext(new LocalMongoConfiguration()));
+            rabbitMqQueue.Received += async eventArgs =>
+            {
+                var username = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+                await userRepository.AddAsync(new User { Name = username });
+            };
+
+            rabbitMqQueue.Consume();
         }
     }
 }
