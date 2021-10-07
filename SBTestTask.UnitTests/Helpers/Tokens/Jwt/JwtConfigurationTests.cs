08e934113f92ac82c134a798bb4cac9000f50cfd
Roman Kovalenko <quadreff.rk@gmail.com>
2021-10-07 07:17:43 +0300

    JwtConfiguration: leftover green tests

--- SBTestTask.UnitTests/Helpers/Tokens/Jwt/JwtConfigurationTests.cs
+++ SBTestTask.UnitTests/Helpers/Tokens/Jwt/JwtConfigurationTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Moq;
 using Microsoft.Extensions.Configuration;
 using Xunit;
@@ -27,6 +28,64 @@ namespace SBTestTask.UnitTests.Helpers.Tokens.Jwt
             // assert
             _configurationMock.Verify();
             actualResult.Secret.Should().BeEquivalentTo(expectedValue);
+        }
+
+        [Fact]
+        public void JwtConfiguration_RetrievesProperIssuer_ReturnsProperIssuer()
+        {
+            // arrange
+            var expectedValue = "test";
+            _configurationMock
+                .SetupGet(x => x[JwtConfiguration.IssuerPath])
+                .Returns(expectedValue)
+                .Verifiable();
+
+            // act
+            var sut = new JwtConfiguration(_configurationMock.Object);
+            var actualResult = sut.Get();
+
+            // assert
+            _configurationMock.Verify();
+            actualResult.Issuer.Should().BeEquivalentTo(expectedValue);
+        }
+
+        [Fact]
+        public void JwtConfiguration_RetrievesProperAudience_ReturnsProperAudience()
+        {
+            // arrange
+            var expectedValue = "test";
+            _configurationMock
+                .SetupGet(x => x[JwtConfiguration.AudiencePath])
+                .Returns(expectedValue)
+                .Verifiable();
+
+            // act
+            var sut = new JwtConfiguration(_configurationMock.Object);
+            var actualResult = sut.Get();
+
+            // assert
+            _configurationMock.Verify();
+            actualResult.Audience.Should().BeEquivalentTo(expectedValue);
+        }
+
+        [Fact]
+        public void JwtConfiguration_RetrievesProperLifetime_ReturnsProperLifetime()
+        {
+            // arrange
+            var lifetime = 5;
+            var expectedValue = TimeSpan.FromMinutes(lifetime);
+            _configurationMock
+                .SetupGet(x => x[JwtConfiguration.LifeTimePath])
+                .Returns(lifetime.ToString)
+                .Verifiable();
+
+            // act
+            var sut = new JwtConfiguration(_configurationMock.Object);
+            var actualResult = sut.Get();
+
+            // assert
+            _configurationMock.Verify();
+            actualResult.Lifetime.Should().Be(expectedValue);
         }
     }
 }
